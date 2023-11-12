f695b8b67cf8ccc228f5f604cbc828cc6e5f7850
zein3 <zeinhaddad02@gmail.com>
2023-11-12 10:40:15 +0700

    ditambah navigation drawer

--- app/src/main/java/com/polstat/singadu/ui/SingaduApp.kt
+++ app/src/main/java/com/polstat/singadu/ui/SingaduApp.kt
@@ -1,25 +1,38 @@
 package com.polstat.singadu.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +44,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.polstat.singadu.R
 import com.polstat.singadu.ui.theme.SingaduTheme
+import kotlinx.coroutines.launch
 
 enum class SingaduScreen() {
     Login,
@@ -44,48 +58,72 @@ fun SingaduApp(
     navController: NavHostController = rememberNavController()
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
     val showTopBar = when (navBackStackEntry?.destination?.route) {
         SingaduScreen.Login.name, SingaduScreen.Register.name -> false
         else -> true
     }
 
-    Scaffold(
-        topBar = {
-            if (showTopBar) {
-                SingaduAppBar()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                SingaduDrawer()
             }
         }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = SingaduScreen.Login.name,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(route = SingaduScreen.Login.name) {
-                LoginScreen(
-                    onLoginButtonClicked = { navController.navigate(SingaduScreen.Home.name) },
-                    onRegisterButtonClicked = { navController.navigate(SingaduScreen.Register.name) }
-                )
-            }
+    ) {
 
-            composable(route = SingaduScreen.Register.name) {
-                RegisterScreen(
-                    onBackButtonClicked = { navController.navigate(SingaduScreen.Login.name) }
-                )
-            }
+        Scaffold(
+            topBar = {
+                if (showTopBar) {
+                    SingaduAppBar(
+                        onMenuClicked = {
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
+                        }
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = SingaduScreen.Login.name,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(route = SingaduScreen.Login.name) {
+                    LoginScreen(
+                        onLoginButtonClicked = { navController.navigate(SingaduScreen.Home.name) },
+                        onRegisterButtonClicked = { navController.navigate(SingaduScreen.Register.name) }
+                    )
+                }
 
-            composable(route = SingaduScreen.Home.name) {
-                HomeScreen()
+                composable(route = SingaduScreen.Register.name) {
+                    RegisterScreen(
+                        onBackButtonClicked = { navController.navigate(SingaduScreen.Login.name) }
+                    )
+                }
+
+                composable(route = SingaduScreen.Home.name) {
+                    HomeScreen()
+                }
             }
         }
+
     }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SingaduAppBar(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onMenuClicked: () -> Unit = {}
 ) {
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.smallTopAppBarColors(
@@ -105,7 +143,7 @@ fun SingaduAppBar(
             }
         },
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onMenuClicked) {
                 Icon(
                     imageVector = Icons.Filled.Menu,
                     contentDescription = stringResource(id = R.string.menu)
@@ -124,10 +162,55 @@ fun SingaduAppBar(
     )
 }
 
+@Composable
+fun SingaduDrawer(
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bps),
+            contentDescription = stringResource(id = R.string.logo_bps),
+            modifier = Modifier.size(32.dp)
+        )
+
+        DrawerNavigationItem(Icons.Filled.Person, R.string.kembali)
+        DrawerNavigationItem(Icons.Filled.Add, R.string.app_name)
+        DrawerNavigationItem(Icons.Filled.Build, R.string.kembali)
+    }
+}
+
+@Composable
+fun DrawerNavigationItem(
+    icons: ImageVector,
+    @StringRes text: Int,
+    onClick: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier.clickable { onClick() }
+    ) {
+        Icon(
+            icons,
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(id = text)
+        )
+    }
+}
+
 @Preview
 @Composable
 fun SingaduAppBarPreview() {
     SingaduTheme {
         SingaduAppBar()
+    }
+}
+
+@Preview
+@Composable
+fun SingaduDrawerPreview() {
+    SingaduTheme {
+        SingaduDrawer()
     }
 }
